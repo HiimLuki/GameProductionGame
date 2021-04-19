69acd6752e0eda630c54078a12f4b60bb68d84a3
Daniel Stehlik <daniel.stehlik@rheelly.net>
2021-04-19 22:19:45 +0200

    Change character movement

--- Assets/PlayerCharacter/PlayerMovement.cs
+++ Assets/PlayerCharacter/PlayerMovement.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("General movement related stuff")]
     [Tooltip("Speed at that character moves")]
     public float movementSpeed;
 
+    [Header("Gravity related stuff")]
     [Tooltip("Gravity that pulls the character down")]
     public float gravity;
+    [Tooltip("Mass of the player character.")]
+    public float mass;
 
     private Vector3 targetPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        targetPosition = transform.position;
+        this.targetPosition = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Player can move character with mouse
         if(Input.GetMouseButton(0))
         {
-            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPosition.z = transform.position.z;
+            this.targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            this.targetPosition.z = this.transform.position.z;
+            this.targetPosition.y = this.transform.position.y;
         }
 
-        targetPosition.y -= gravity;
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementSpeed * Time.deltaTime);
+        // Apply gravity to character
+        this.targetPosition.y -= gravity;
+
+        // Apply new position to character
+        this.transform.position = Vector3.MoveTowards(this.transform.position, this.targetPosition, this.movementSpeed * Time.deltaTime);
     }
 }
