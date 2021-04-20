53ffa54f9b12a3dfa53239b82600f53627636fc8
HiimLuki <xmrviruzx@gmail.com>
2021-04-20 11:48:50 +0200

    Added CameraMovement, DeadBox and script on Player to get all Variables

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,12 +7,28 @@ public class CameraController : MonoBehaviour
 
     //Look At Variable
     public Transform target;
+    private float speed = 0.1f;
+
+    //Reference to all Player Variables
+    public PlayerVariables variables;
     
     //Update the Position of the Camera
     public void Update()
     {
-        Vector3 targetPos = new Vector3(0, target.position.y, transform.position.z);
-
-        transform.position = targetPos;
+ 
+        if(variables.rb.velocity.y <= -2)
+        {
+            speed += 0.001f;
+            Vector3 targetPos = new Vector3(0, target.position.y + speed, transform.position.z);
+            transform.position = targetPos;
+        }
+        else
+        {
+            speed -= 0.001f;
+            Vector3 targetPos = new Vector3(0, target.position.y + speed, transform.position.z);
+            transform.position = targetPos;
+        }
+           
     }
+
 }
