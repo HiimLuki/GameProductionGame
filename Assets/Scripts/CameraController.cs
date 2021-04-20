b3bed00f6b94e5eff7afc410b2661155f4913b09
HiimLuki <xmrviruzx@gmail.com>
2021-04-20 11:54:31 +0200

    Deleted Variable script added RB in cameracontroller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,14 +9,13 @@ public class CameraController : MonoBehaviour
     public Transform target;
     private float speed = 0.1f;
 
-    //Reference to all Player Variables
-    public PlayerVariables variables;
+    public Rigidbody2D rb;
     
     //Update the Position of the Camera
     public void Update()
     {
  
-        if(variables.rb.velocity.y <= -2)
+        if(rb.velocity.y <= -2)
         {
             speed += 0.001f;
             Vector3 targetPos = new Vector3(0, target.position.y + speed, transform.position.z);
