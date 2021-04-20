9b099663156856cce028814790b6ac16c6322bed
HiimLuki <xmrviruzx@gmail.com>
2021-04-20 12:55:17 +0200

    Changes on Camera
    Added Camera Prefab, changed Camera Controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,25 +9,56 @@ public class CameraController : MonoBehaviour
     public Transform target;
     private float speed = 0.1f;
 
+    //Variable to get Velocity
     public Rigidbody2D rb;
+
+    public Transform tile1;
+    public Transform tile2;
+
+    private float size;
+
+    public void Start()
+    {
+        size = tile1.GetComponent<BoxCollider2D>().size.y;
+    }
     
     //Update the Position of the Camera
     public void Update()
     {
  
+        //Make Camera Slower Faster in context to the Playerspeed
         if(rb.velocity.y <= -2)
         {
+            //Camera goes slower
             speed += 0.001f;
             Vector3 targetPos = new Vector3(0, target.position.y + speed, transform.position.z);
             transform.position = targetPos;
         }
         else
         {
+            //Camera goes faster
             speed -= 0.001f;
             Vector3 targetPos = new Vector3(0, target.position.y + speed, transform.position.z);
             transform.position = targetPos;
         }
+
+
+
+        //Generate new Tile
+        if(transform.position.y < tile1.position.y)
+        {
+            tile2.position = new Vector3(tile2.position.x, tile1.position.y - size, tile2.position.z);
+            SwitchTile();
+        }
            
+    }
+
+    //Switch the Tiles
+    private void SwitchTile()
+    {
+        Transform temp = tile1;
+        tile1 = tile2;
+        tile2 = temp;
     }
 
 }
